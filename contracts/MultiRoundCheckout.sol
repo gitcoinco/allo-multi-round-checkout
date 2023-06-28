7d170ae962e94204c26b6f2f9985f51d67438f25
Atris <vacekj@outlook.com>
2023-06-28 15:30:49 +0200

    feat: test setup working

--- contracts/MultiRoundCheckout.sol
+++ contracts/MultiRoundCheckout.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "../allo-contracts/contracts/round/RoundImplementation.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
-contract MultiRoundCheckout is OwnableUpgradeable {
+contract MultiRoundCheckout is OwnableUpgradeable, ReentrancyGuardUpgradeable {
     /**
-    * votes is a 2d array. first index is the address of the round.
+    * votes is a 2d array. first index is the inde of the round address in the second param.
     */
-    function vote(bytes[][] calldata votes, address[] calldata rounds) public {
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    function vote(bytes[][] memory votes, address[] memory rounds) public nonReentrant {
         for (uint i = 0; i < rounds.length; i++) {
-            RoundImplementation round = RoundImplementation(payable(rounds[i]));
-            bytes[] memory votesForRound = votes[i];
-            round.vote(votesForRound);
+            IRoundImplementation round = IRoundImplementation(payable(rounds[i]));
+            round.vote(votes[i]);
         }
     }
+}
+
+interface IRoundImplementation {
+    function vote(bytes[] memory data) external payable;
 }
--- test/MultiRoundCheckout.t.sol
+++ test/MultiRoundCheckout.t.sol
@@ -1,128 +1,64 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import "../contracts/MultiRoundCheckout.sol";
-import "../allo-contracts/contracts/votingStrategy/QuadraticFundingStrategy/QuadraticFundingVotingStrategyFactory.sol";
-import "../allo-contracts/contracts/payoutStrategy/MerklePayoutStrategy/MerklePayoutStrategyFactory.sol";
-import "../allo-contracts/contracts/round/RoundFactory.sol";
 
-contract ContractBTest is Test {
+contract MrcTest is Test {
     MultiRoundCheckout private mrc;
-    RoundImplementation private round1;
-    RoundImplementation private round2;
-    RoundImplementation private round3;
-
-    struct InitAddress {
-        IVotingStrategy votingStrategy; // Deployed voting strategy contract
-        IPayoutStrategy payoutStrategy; // Deployed payout strategy contract
-    }
-
-    struct InitRoundTime {
-        uint256 applicationsStartTime; // Unix timestamp from when round can accept applications
-        uint256 applicationsEndTime; // Unix timestamp from when round stops accepting applications
-        uint256 roundStartTime; // Unix timestamp of the start of the round
-        uint256 roundEndTime; // Unix timestamp of the end of the round
-    }
-
-    struct InitMetaPtr {
-        MetaPtr roundMetaPtr; // MetaPtr to the round metadata
-        MetaPtr applicationMetaPtr; // MetaPtr to the application form schema
-    }
-
-    struct InitRoles {
-        address[] adminRoles; // Addresses to be granted DEFAULT_ADMIN_ROLE
-        address[] roundOperators; // Addresses to be granted ROUND_OPERATOR_ROLE
-    }
+    MockRoundImplementation private round1;
+    MockRoundImplementation private round2;
+    MockRoundImplementation private round3;
 
     function setUp() public {
+        round1 = new MockRoundImplementation();
+        round2 = new MockRoundImplementation();
+        round3 = new MockRoundImplementation();
         mrc = new MultiRoundCheckout();
-
-        QuadraticFundingVotingStrategyFactory qfVotingStrategyFactory = new QuadraticFundingVotingStrategyFactory();
-        qfVotingStrategyFactory.initialize();
-
-        QuadraticFundingVotingStrategyImplementation qfVotingStratImpl = new QuadraticFundingVotingStrategyImplementation();
-        qfVotingStratImpl.initialize();
-
-        qfVotingStrategyFactory.updateVotingContract(
-            address(qfVotingStratImpl)
-        );
-
-        MerklePayoutStrategyFactory merkleFactory = new MerklePayoutStrategyFactory();
-        merkleFactory.initialize();
-
-        MerklePayoutStrategyImplementation merkleImpl = new MerklePayoutStrategyImplementation();
-        merkleImpl.initialize();
-
-        merkleFactory.updatePayoutImplementation(payable(merkleImpl));
-
-        AlloSettings settings = new AlloSettings();
-        settings.initialize();
-
-        RoundFactory roundFactory = new RoundFactory();
-        roundFactory.initialize();
-
-        RoundImplementation roundImpl = new RoundImplementation();
-
-        roundFactory.updateRoundImplementation(payable(roundImpl));
-        roundFactory.updateAlloSettings(address(settings));
-
-        bytes memory params = generateAndEncodeRoundParam(
-            address(qfVotingStratImpl),
-            payable(merkleImpl),
-            msg.sender
-        );
-
-        roundFactory.create(params, msg.sender);
     }
 
-    function generateAndEncodeRoundParam(
-        address votingContract,
-        address payable payoutContract,
-        address adminAddress
-    ) public view returns (bytes memory) {
-        uint256 currentTimestamp = block.timestamp;
-        MetaPtr memory roundMetaPtr = MetaPtr(
-            1,
-            "bafybeia4khbew3r2mkflyn7nzlvfzcb3qpfeftz5ivpzfwn77ollj47gqi"
-        );
-        MetaPtr memory applicationMetaPtr = MetaPtr(
-            1,
-            "bafkreih3mbwctlrnimkiizqvu3zu3blszn5uylqts22yvsrdh5y2kbxaia"
-        );
-        address[] memory roles = new address[](1);
-        roles[0] = adminAddress;
-        uint256 matchAmount = 100;
-        address token = address(0);
-        uint32 roundFeePercentage = 0;
-        address roundFeeAddress = address(0);
-        InitAddress memory initAddress = InitAddress(
-            QuadraticFundingVotingStrategyImplementation(votingContract),
-            MerklePayoutStrategyImplementation(payoutContract)
-        );
-        uint256 SECONDS_PER_SLOT = 15; // Goerli slot time
-        InitRoundTime memory initRoundTime = InitRoundTime(
-            currentTimestamp, // Applications start
-            currentTimestamp + 60, // Applications End
-            currentTimestamp, // Voting Starts
-            currentTimestamp + 120 // Voting Ends
-        );
-        InitMetaPtr memory initMetaPtr = InitMetaPtr(
-            roundMetaPtr,
-            applicationMetaPtr
-        );
-        InitRoles memory initRoles = InitRoles(roles, roles);
+    function testVotesPassing() public {
+        address[] memory rounds = new address[](3);
+        rounds[0] = address(round1);
+        rounds[1] = address(round2);
+        rounds[2] = address(round3);
 
-        return
-            abi.encode(
-            initAddress,
-            initRoundTime,
-            matchAmount,
-            token,
-            roundFeePercentage,
-            roundFeeAddress,
-            initMetaPtr,
-            initRoles
-        );
+        bytes[][] memory votes = new bytes[][](3);
+
+        votes[0] = new bytes[](3);
+        votes[0][0] = "A";
+        votes[0][1] = "B";
+        votes[0][2] = "C";
+
+        votes[1] = new bytes[](3);
+        votes[1][0] = "A";
+        votes[1][1] = "B";
+        votes[1][2] = "C";
+
+        votes[2] = new bytes[](3);
+        votes[2][0] = "A";
+        votes[2][1] = "B";
+        votes[2][2] = "C";
+
+        mrc.vote(votes, rounds);
+
+        for (uint i = 0; i < rounds.length; i++) {
+            bytes[] memory receivedVotes = MockRoundImplementation(rounds[i]).getReceivedVotes();
+            for (uint j = 0; j < receivedVotes.length; j++) {
+                assertEq(receivedVotes[j],votes[i][j]);
+            }
+        }
+    }
+}
+
+contract MockRoundImplementation is IRoundImplementation {
+    bytes[] public receivedVotes;
+
+    function vote(bytes[] memory data) external payable {
+        receivedVotes = data;
+    }
+
+    function getReceivedVotes() public returns (bytes[] memory) {
+        return receivedVotes;
     }
 }
