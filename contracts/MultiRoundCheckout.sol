6d689e3bcabb29b9e4f5e7daec0509d3a07db3c2
Atris <vacekj@outlook.com>
2023-06-28 11:57:42 +0200

    feat: foundry

--- contracts/MultiRoundCheckout.sol
+++ contracts/MultiRoundCheckout.sol
@@ -1,25 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "../allo-contracts/contracts/round/IRoundImplementation.sol";
 import "hardhat/console.sol";
 
 contract MultiRoundCheckout is OwnableUpgradeable {
-    address payable public owner;
-
-    constructor(uint _unlockTime) payable {
-        owner = payable(msg.sender);
-    }
-
     /**
     * votes is a 2d array. first index is the address of the round.
     */
-    function vote(bytes[][] calldata votes, address[] rounds) public {
+    function vote(bytes[][] calldata votes, address[] calldata rounds) public {
         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
         for (uint i = 0; i < rounds.length; i++) {
-            IRoundImplementation round = address(round[i]);
-            bytes[] votesForRound = votes[i];
+            IRoundImplementation round = IRoundImplementation(rounds[i]);
+            bytes[] memory votesForRound = votes[i];
             round.vote(votesForRound);
         }
     }
