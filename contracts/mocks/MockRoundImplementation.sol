5728c4c7f33135003073abbb369d5341d9dac750
bhargavaparoksham <aparokshambhargav@gmail.com>
2023-07-03 20:04:20 +0530

    feat: add MockRoundImplementation

---
+++ contracts/mocks/MockRoundImplementation.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "../MultiRoundCheckout.sol";
+
+contract MockRoundImplementation is IVotable {
+    bytes[] public receivedVotes;
+    bool public tryReentrancy;
+
+    function setReentrant(bool _tryReentrancy) public {
+        tryReentrancy = _tryReentrancy;
+    }
+
+    function vote(bytes[] memory data) external payable {
+        if (tryReentrancy)  {
+            address[] memory rounds = new address[](1);
+            bytes[][] memory votes = new bytes[][](1);
+            uint256[] memory values = new uint256[](1);
+            rounds[0] = address(this);
+            values[0] = msg.value;
+            MultiRoundCheckout(msg.sender).vote{value: msg.value}(votes, rounds, values);
+        }
+        receivedVotes = data;
+    }    
+
+    function getReceivedVotes() public view returns (bytes[] memory) {
+        return receivedVotes;
+    }
+}
