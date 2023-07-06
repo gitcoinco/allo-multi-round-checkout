12db802cad1cd258a8b38e89a0a12d2d39008d31
bhargavaparoksham <aparokshambhargav@gmail.com>
2023-07-06 12:46:39 +0530

    feat: support vote with ERC20Permit

--- contracts/MultiRoundCheckout.sol
+++ contracts/MultiRoundCheckout.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/IERC20PermitUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "./IVotable.sol";
 
 error VotesNotEqualRoundsLength();
@@ -30,6 +32,7 @@ contract MultiRoundCheckout is
     }
 
     /**
+     * vote: votes for multiple rounds at once with ETH.
      * votes is a 2d array. first index is the index of the round address in the second param.
      */
     function vote(
@@ -51,6 +54,49 @@ contract MultiRoundCheckout is
         }
 
         if (address(this).balance != 0) {
+            revert ExcessAmountSent();
+        }
+    }
+
+
+    /**
+     * voteERC20Permit: votes for multiple rounds at once with ERC20Permit tokens.
+     */
+    function voteERC20Permit(
+        bytes[][] memory votes,
+        address[] memory rounds,
+        uint256 totalAmount,
+        address token,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public nonReentrant whenNotPaused {
+        if (votes.length != rounds.length) {
+            revert VotesNotEqualRoundsLength();
+        }
+
+        if (amounts.length != rounds.length) {
+            revert AmountsNotEqualRoundsLength();
+        }
+
+        IERC20PermitUpgradeable(token).permit(
+            msg.sender,
+            address(this),
+            totalAmount,
+            type(uint256).max,
+            v,
+            r,
+            s
+        );
+
+        IERC20Upgradeable(token).transferFrom(msg.sender, address(this), totalAmount);
+
+        for (uint i = 0; i < rounds.length; i++) {
+            IVotable round = IVotable(payable(rounds[i]));
+            round.vote(votes[i]);
+        }
+
+        if (IERC20Upgradeable(token).balanceOf(address(this)) != 0) {
             revert ExcessAmountSent();
         }
     }
