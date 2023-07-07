214c7cf5985bd7c2aebc02d245ef11aab6d843d1
bhargavaparoksham <aparokshambhargav@gmail.com>
2023-07-07 23:06:37 +0530

    feat: update MockERC20Permit

--- contracts/mocks/MockERC20Permit.sol
+++ contracts/mocks/MockERC20Permit.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
 
-contract MockERC20Permit is ERC20Upgradeable, ERC20PermitUpgradeable {
+contract MockERC20Permit is ERC20PermitUpgradeable {
 
     function initialize(string memory name, string memory symbol) public initializer {
         __ERC20_init(name, symbol);
