e7407e691e967a6a7edd988287c9eda180831214
bhargavaparoksham <aparokshambhargav@gmail.com>
2023-07-07 19:55:09 +0530

    feat: update IVotable

--- contracts/IVotable.sol
+++ contracts/IVotable.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.17;
 
-interface IVotable {
-    function vote(bytes[] memory data) external payable;
+abstract contract IVotable {
+    address public votingStrategy;
+    function vote(bytes[] memory data) external virtual payable;
 }
