7f61fbf5fec0d9cbbb89416773272042d44a0805
Andrea Franz <andrea@gravityblast.com>
2023-07-25 23:58:45 +0200

    add deploy script

---
+++ lib/utils.ts
@@ -0,0 +1,30 @@
+const rl = require("readline");
+const { BigNumber } = require("@ethersproject/bignumber");
+
+const BN = (n: any) => BigNumber.from(n.toString());
+
+export const prompt = async (question: string) => {
+  const r = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  });
+
+  const answer = await new Promise((resolve, error) => {
+    r.question(`${question} [y/n]: `, (answer: string) => {
+      r.close();
+      resolve(answer);
+    });
+  });
+
+  if (answer !== "y" && answer !== "yes") {
+    console.log("exiting...");
+    process.exit(1);
+  }
+
+  console.log();
+};
+
+// pretty number
+export const pn = (n: bigint | string) =>
+  n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "_");
